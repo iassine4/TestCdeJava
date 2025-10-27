819e17e34f844ecff15dbfa4e1175ae2a19b9d66
Yassine <yassine.zriba@fms-ea.com>
2025-10-27 14:46:59 +0100

    Add quel est votre nom ? + enter the name then display salut nom

--- Hello.java
+++ Hello.java
@@ -1,6 +1,11 @@
 public class Hello {
 
     public static void main(String[] args) {
+        java.util.Scanner scan = new java.util.Scanner(System.in);
         System.out.println("bonjour et bienvenu dans mon programme java");
+        System.out.print("Quel est votre nom ? ");
+        String nom = scan.nextLine();
+
+        System.out.println("Salut " + nom.toUpperCase() + " !");
     }
 }
