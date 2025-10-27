351379ab8ffe4a5ab8ff8d63a525e24e46aff724
Yassine <yassine.zriba@fms-ea.com>
2025-10-27 14:53:01 +0100

    Add scanner prenom to get bienvenu nom prénom

--- Hello.java
+++ Hello.java
@@ -2,10 +2,15 @@ public class Hello {
 
     public static void main(String[] args) {
         java.util.Scanner scan = new java.util.Scanner(System.in);
+        
         System.out.println("bonjour et bienvenu dans mon programme java");
         System.out.print("Quel est votre nom ? ");
         String nom = scan.nextLine();
 
+        System.out.print("Quel est votre prénom ? ");
+        String prenom = scan.nextLine();
+
         System.out.println("Salut " + nom.toUpperCase() + " !");
+        System.out.println("Bienvenue " + nom.toUpperCase() + " " + prenom.toLowerCase() + " !");
     }
 }
